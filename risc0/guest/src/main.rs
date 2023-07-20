15eecc99834416e60c266cc49850459f565d030f
Adam Fiedler <info@fiedlr.sk>
2023-07-21 00:28:40 +0200

    Add the bytestream implementation for proof input

--- risc0/guest/src/main.rs
+++ risc0/guest/src/main.rs
@@ -1,13 +1,56 @@
 #![no_main]
 #![no_std]
 
-extern crate checker;
-use checker::test_phi_implies_phi;
-
-use risc0_zkvm::guest::env;
-
+use risc0_zkvm::guest::env::{self, FdReader};
 risc0_zkvm::guest::entry!(main);
 
+use risc0_zkvm::serde::WordRead;
+use risc0_zkvm::serde::Error::DeserializeUnexpectedEnd;
+use risc0_zkvm::sha::WORD_SIZE;
+
+extern crate checker;
+use checker::{verify, Instruction};
+
+struct SimpleBuffer<'a> {
+    input: &'a mut FdReader,
+    v: [u8; WORD_SIZE],
+    index: usize
+}
+
+const BUFFER_ERROR : u8 = 99;
+
+fn read_from(input: &mut FdReader, buffer: &mut [u8]) -> u8 {
+    // Pads with 0, but it cannot happen we take it as an argument because input needs to contain a BUFFER_EOF
+    let read_buffer = (*input).read_padded_bytes(buffer);
+    match read_buffer {
+      Ok(_r) => (*buffer)[0],
+      Err(r) => if r == DeserializeUnexpectedEnd { Instruction::EOF as u8 } else { BUFFER_ERROR }
+    }
+}
+
+impl<'a> SimpleBuffer<'a> {
+    fn next(&mut self) -> Option<u8> {
+        if self.index < WORD_SIZE {
+          self.index += 1;
+          return Some(self.v[self.index - 1]);
+        } else {
+          read_from(self.input, &mut self.v);
+          self.index = 1;
+          return Some(self.v[0]);
+        }
+    }
+}
+
 pub fn main() {
-    test_phi_implies_phi();
+    let mut stdin_stream = SimpleBuffer {
+        input: &mut env::stdin(),
+        v: [0; WORD_SIZE],
+        index: WORD_SIZE
+    };
+
+    let next = &mut (|| stdin_stream.next());
+
+    verify(next);
+
+    env::commit(&env::get_cycle_count());
 }
--- risc0/src/main.rs
+++ risc0/src/main.rs
@@ -6,10 +6,96 @@ use risc0_zkvm::{
     ExecutorEnv,
 };
 
+extern crate checker;
+use checker::Instruction;
+use std::time::Instant;
+
 fn main() {
-    let env = ExecutorEnv::builder().build().unwrap();
+    let now = Instant::now();
+
+    let proof: &[u8] = &[
+        Instruction::Prop1 as u8,               // (p1: phi0 -> (phi1 -> phi0))
+
+        Instruction::List as u8, 0 as u8,
+        Instruction::List as u8, 0 as u8,
+        Instruction::List as u8, 0 as u8,
+        Instruction::List as u8, 0 as u8,
+        Instruction::List as u8, 0 as u8,
+        Instruction::MetaVar as u8, 1 as u8,          // Stack: p1 ; phi1
+        Instruction::Save as u8,                // phi1 save at 0
+
+        Instruction::List as u8, 0 as u8,
+        Instruction::List as u8, 0 as u8,
+        Instruction::List as u8, 0 as u8,
+        Instruction::List as u8, 0 as u8,
+        Instruction::List as u8, 0 as u8,
+        Instruction::MetaVar as u8, 0 as u8,          // Stack: p1 ; phi1 ; phi0
+        Instruction::Save as u8,                // phi0 save at 1
+
+        Instruction::InstantiateSchema as u8,   // Stack: (p2: phi0 -> (phi0 -> phi0))
+
+        Instruction::Prop1 as u8,               // Stack: p2 ; p1
+        Instruction::Load as u8, 0 as u8,             // Stack: p2 ; p1 ; phi1
+        Instruction::Load as u8, 1 as u8,             // Stack: p2 ; p1 ; phi0
+        Instruction::Load as u8, 1 as u8,             // Stack: p2 ; p1 ; phi0 ; phi0
+        Instruction::Implication as u8,         // Stack: p2 ; p1 ; phi1; phi0 -> phi0
+
+        Instruction::Save as u8,                // phi0 -> phi0 save at 3
+
+        Instruction::InstantiateSchema as u8,   // Stack: p2 ; (p3: phi0 -> (phi0 -> phi0) -> phi0)
+
+        Instruction::Prop2 as u8,               // Stack: p2 ; p3; (p4: (phi0 -> (phi1 -> phi2)) -> ((phi0 -> phi1) -> (phi0 -> phi2))
+        Instruction::Load as u8, 0 as u8,
+        Instruction::Load as u8, 2 as u8,
+        Instruction::InstantiateSchema as u8,
+
+        Instruction::List as u8, 0 as u8,
+        Instruction::List as u8, 0 as u8,
+        Instruction::List as u8, 0 as u8,
+        Instruction::List as u8, 0 as u8,
+        Instruction::List as u8, 0 as u8,
+        Instruction::MetaVar as u8, 2 as u8,
+        Instruction::Load as u8, 1 as u8,
+        Instruction::InstantiateSchema as u8,
+
+        Instruction::ModusPonens as u8,
+        Instruction::ModusPonens as u8,         // Stack: phi0 -> phi0
+
+        Instruction::EOF as u8,                                    // EOF
+    ];
+
+    println!("Setting up env...");
+
+    // First, we construct an executor environment
+    let env = ExecutorEnv::builder()
+        .stdin(proof)
+        .build()
+        .unwrap();
+
+    // Next, we make an executor, loading the (renamed) ELF binary.
     let mut exec = default_executor_from_elf(env, GUEST_ELF).unwrap();
+
+    println!("Checking the proof...");
+
+    // Run the executor to produce a session.
     let session = exec.run().unwrap();
+
+    println!("Ran in {} s", now.elapsed().as_secs());
+
+    println!("Generating the certificate...");
+
+    // Prove the session to produce a receipt.
     let receipt = session.prove().unwrap();
+
+    // TODO: Implement code for transmitting or serializing the receipt for
+    // other parties to verify here
+    let theorem: usize = from_slice(&receipt.journal).unwrap();
+    println!("We can prove the theorem in {} cycles and have a ZK certificate for it!", theorem);
+
+    // Optional: Verify receipt to confirm that recipients will also be able to
+    // verify your receipt
     receipt.verify(GUEST_ID).unwrap();
+
+    println!("Running execution + ZK certficate generation + verification took {} s", now.elapsed().as_secs());
 }
+
