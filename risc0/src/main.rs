a9d419c0838c6c3e027b0d5eb841c80b6088b8c8
Nishant Rodrigues <nishantjr@gmail.com>
2023-07-28 17:48:29 -0500

    cargo fmt

--- risc0/src/main.rs
+++ risc0/src/main.rs
@@ -1,10 +1,6 @@
 use zk_host::methods::{GUEST_ELF, GUEST_ID};
 
-use risc0_zkvm::{
-    default_executor_from_elf,
-    serde::from_slice,
-    ExecutorEnv,
-};
+use risc0_zkvm::{default_executor_from_elf, serde::from_slice, ExecutorEnv};
 
 use std::fs::File;
 use std::io::BufReader;
@@ -20,10 +16,7 @@ fn main() {
     let reader = BufReader::new(f);
 
     // First, we construct an executor environment
-    let env = ExecutorEnv::builder()
-        .stdin(reader)
-        .build()
-        .unwrap();
+    let env = ExecutorEnv::builder().stdin(reader).build().unwrap();
 
     // Next, we make an executor, loading the (renamed) ELF binary.
     let mut exec = default_executor_from_elf(env, GUEST_ELF).unwrap();
@@ -43,12 +36,17 @@ fn main() {
     // TODO: Implement code for transmitting or serializing the receipt for
     // other parties to verify here
     let theorem: usize = from_slice(&receipt.journal).unwrap();
-    println!("We can prove the theorem in {} cycles and have a ZK certificate for it!", theorem);
+    println!(
+        "We can prove the theorem in {} cycles and have a ZK certificate for it!",
+        theorem
+    );
 
     // Optional: Verify receipt to confirm that recipients will also be able to
     // verify your receipt
     receipt.verify(GUEST_ID).unwrap();
 
-    println!("Running execution + ZK certficate generation + verification took {} s", now.elapsed().as_secs());
+    println!(
+        "Running execution + ZK certficate generation + verification took {} s",
+        now.elapsed().as_secs()
+    );
 }
-
