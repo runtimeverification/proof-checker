626d76b1562cf1a277006d1c7ad1e2d740e0b9b9
Nishant Rodrigues <nishantjr@gmail.com>
2023-07-28 14:47:01 -0500

    generation: Move imp_reflexivity into class Propositional

---
+++ generation/src/proof_generation/proofs/propositional.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from proof_generation.proof import MetaVar, implies, modus_ponens, prop1, prop2
+
+if TYPE_CHECKING:
+    from proof_generation.proof import Pattern, Proof
+
+
+class Propositional:
+    def theory(self) -> list[Pattern]:
+        """Returns a list of axioms. In this case we do not rely on any axioms."""
+        return []
+
+    def lemmas(self) -> set[Pattern]:
+        """Returns a list statements for lemmas that we should reuse."""
+        return set()
+
+    def notation(self) -> set[Pattern]:
+        """Returns a list patterns that we will want to reuse."""
+        return {self.phi0}
+
+    def published(self) -> set[Pattern]:
+        """Returns a list statements for theorems that we want to publish."""
+        return {self.phi0_implies_phi0}
+
+    def proof(self) -> list[Proof]:
+        """Returns a list of proofs for the published statements."""
+        return [self.imp_reflexivity]
+
+    phi0: MetaVar = MetaVar(0)
+    phi1: MetaVar = MetaVar(1)
+    phi2: MetaVar = MetaVar(2)
+    phi0_implies_phi0: Pattern = implies(phi0, phi0)
+    imp_reflexivity: Proof = modus_ponens(
+        prop1.instantiate(phi1, phi0),
+        modus_ponens(
+            prop1.instantiate(phi1, phi0_implies_phi0),
+            prop2.instantiate(phi1, phi0_implies_phi0).instantiate(phi2, phi0),
+        ),
+    )
--- generation/src/tests/unit/test_proof.py
+++ generation/src/tests/unit/test_proof.py
@@ -3,15 +3,12 @@ from __future__ import annotations
 from io import BytesIO
 
 from proof_generation.instruction import Instruction
-from proof_generation.proof import MetaVar, implies, modus_ponens, prop1, prop2
+from proof_generation.proofs.propositional import Propositional
 
 
 def test_serialize_phi_implies_phi() -> None:
-    phi0 = MetaVar(0)
-    phi_implies_phi = implies(phi0, phi0)
-
     out = BytesIO()
-    phi_implies_phi.serialize({phi0}, [], out)
+    Propositional.phi0_implies_phi0.serialize({Propositional.phi0}, [], out)
     # fmt: off
     assert bytes(out.getbuffer()) == bytes([
         Instruction.List, 0,
@@ -28,19 +25,8 @@ def test_serialize_phi_implies_phi() -> None:
 
 
 def test_prove_imp_implies_imp() -> None:
-    phi0 = MetaVar(0)
-    phi1 = MetaVar(1)
-    phi2 = MetaVar(2)
-    phi0_implies_phi0 = implies(phi0, phi0)
-    imp_reflexivity = modus_ponens(
-        prop1.instantiate(phi1, phi0),
-        modus_ponens(
-            prop1.instantiate(phi1, phi0_implies_phi0),
-            prop2.instantiate(phi1, phi0_implies_phi0).instantiate(phi2, phi0),
-        ),
-    )
     out = BytesIO()
-    imp_reflexivity.serialize({phi0, phi0_implies_phi0}, [], out)
+    Propositional.imp_reflexivity.serialize({Propositional.phi0, Propositional.phi0_implies_phi0}, [], out)
     # fmt: off
     assert bytes(out.getbuffer()) == bytes([
         Instruction.Prop1,              # (p1: phi0 -> (phi1 -> phi0))
