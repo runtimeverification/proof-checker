2cef4958d80d4ad4b33a6b2b2ca1f9444a47f506
Nishant Rodrigues <nishantjr@gmail.com>
2023-08-07 16:47:20 -0500

    checker: Implement Mu instruction

--- checker/src/lib.rs
+++ checker/src/lib.rs
@@ -474,6 +474,11 @@ fn execute_instructions<'a>(
                 let subpattern = pop_stack_pattern(stack);
                 stack.push(Term::Pattern(exists(id, subpattern)))
             }
+            Instruction::Mu => {
+                let id = next().expect("Expected var_id for the exists binder");
+                let subpattern = pop_stack_pattern(stack);
+                stack.push(Term::Pattern(exists(id, subpattern)))
+            }
             Instruction::MetaVar => {
                 let id = next().expect("Insufficient parameters for MetaVar instruction");
                 let application_context = pop_stack_list(stack);
