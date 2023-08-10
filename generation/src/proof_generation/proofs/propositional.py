bd0a24cbf846e3b16363a7b71b8c4e2b2c43e0da
Adam Fiedler <info@fiedlr.sk>
2023-08-10 20:38:38 +0200

    Change tuple to tuple literal

--- generation/src/proof_generation/proofs/propositional.py
+++ generation/src/proof_generation/proofs/propositional.py
@@ -77,10 +77,10 @@ class Propositional(ProofExp):
     # phi0 -> phi0
     def imp_reflexivity(self) -> Proof:
         return modus_ponens(
-            prop1.instantiate(tuple([1]), tuple([self.phi0])),
+            prop1.instantiate((1,), (self.phi0,)),
             modus_ponens(
-                prop1.instantiate(tuple([1]), tuple([self.phi0_implies_phi0])),
-                prop2.instantiate(tuple([1, 2]), tuple([self.phi0_implies_phi0, self.phi0])),
+                prop1.instantiate((1,), (self.phi0_implies_phi0,)),
+                prop2.instantiate((1, 2), (self.phi0_implies_phi0, self.phi0)),
             ),
             self.prop1().instantiate(1, self.phi0),
         )
