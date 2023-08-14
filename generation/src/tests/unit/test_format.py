a6ef2c676eddbec20362d028ea8e9311b36fa5e7
Nishant Rodrigues <nishantjr@gmail.com>
2023-08-14 12:45:43 -0500

    generation: test_format: better formatting
    Please ignore the auto-formatter when it makes things uglier

--- generation/src/tests/unit/test_format.py
+++ generation/src/tests/unit/test_format.py
@@ -24,20 +24,58 @@ imp_refl = Propositional().imp_reflexivity()
 
 @pytest.mark.parametrize(
     'formatter, input_, expected',
+    # fmt: off
     [
-        (mlp, not_phi0, '¬φ0'),
-        (mlp, some_diverse_pattern, 'μ(SVar(0),\n  ∃(EVar(0),\n     ¬φ25(e_f=(EVar(5),))))'),
-        (
-            mlp,
-            imp_refl,
-            'MP(Inst(Prop1(),\n         1,\n         φ0),\n   MP(Inst(Prop1(),\n             1,\n             Imp(φ0,\n                  φ0)),\n       Inst(Inst(Prop2(),\n                   1,\n                   Imp(φ0,\n                        φ0)),\n             2,\n             φ0)))',
+        ( mlp, not_phi0, '¬φ0' ),
+        ( mlp, some_diverse_pattern,
+          'μ(SVar(0),\n'
+          '  ∃(EVar(0),\n'
+          '     ¬φ25(e_f=(EVar(5),))))'),
+        ( mlp,
+          imp_refl,
+          'MP(Inst(Prop1(),\n'
+          '         1,\n'
+          '         φ0),\n'
+          '   MP(Inst(Prop1(),\n'
+          '             1,\n'
+          '             Imp(φ0,\n'
+          '                  φ0)),\n'
+          '       Inst(Inst(Prop2(),\n'
+          '                   1,\n'
+          '                   Imp(φ0,\n'
+          '                        φ0)),\n'
+          '             2,\n'
+          '             φ0)))',
         ),
         (
-            mlpd,
-            imp_refl,
-            'MP(Inst(Imp(φ0,\n              Imp(φ1,\n                   φ0)),\n         1,\n         φ0),\n   MP(Inst(Imp(φ0,\n                  Imp(φ1,\n                       φ0)),\n             1,\n             Imp(φ0,\n                  φ0)),\n       Inst(Inst(Imp(Imp(φ0,\n                             Imp(φ1,\n                                  φ2)),\n                        Imp(Imp(φ0,\n                                  φ1),\n                             Imp(φ0,\n                                  φ2))),\n                   1,\n                   Imp(φ0,\n                        φ0)),\n             2,\n             φ0)))',
+          mlpd,
+          imp_refl,
+          'MP(Inst(Imp(φ0,\n'
+          '              Imp(φ1,\n'
+          '                   φ0)),\n'
+          '         1,\n'
+          '         φ0),\n'
+          '   MP(Inst(Imp(φ0,\n'
+          '                  Imp(φ1,\n'
+          '                       φ0)),\n'
+          '             1,\n'
+          '             Imp(φ0,\n'
+          '                  φ0)),\n'
+          '       Inst(Inst(Imp(Imp(φ0,\n'
+          '                             Imp(φ1,\n'
+          '                                  φ2)),\n'
+          '                        Imp(Imp(φ0,\n'
+          '                                  φ1),\n'
+          '                             Imp(φ0,\n'
+          '                                  φ2))),\n'
+          '                   1,\n'
+          '                   Imp(φ0,\n'
+          '                        φ0)),\n'
+          '             2,\n'
+          '             φ0)))',
         ),
     ],
+    # fmt: on
 )
 def test_pformat(formatter: PrettyPrinter, input_: Proof, expected: str) -> None:
     assert formatter.pformat(input_) == expected
