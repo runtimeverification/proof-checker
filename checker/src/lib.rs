0659d8207c5e274d48d9955eafc46ab754f769cb
Adam Fiedler <info@fiedlr.sk>
2023-08-15 18:40:56 +0200

    Add first tests on simultaneous instantiations

--- checker/src/lib.rs
+++ checker/src/lib.rs
@@ -944,6 +944,7 @@ fn test_wellformedness_instantiate() {
     let appphi0phi0 = app(Rc::clone(&x0), Rc::clone(&x0));
     let existsx0phi0 = exists(0, Rc::clone(&phi0));
     let muX0phi0 = mu(0, Rc::clone(&phi0));
+    let existsx0X0 = exists(0, Rc::clone(&X0));
     assert!(instantiate(Rc::clone(&phi0_implies_phi0), &[0], &[Rc::clone(&x0)]) == x0_implies_x0);
     assert!(
         instantiate(Rc::clone(&phi0_implies_phi0), &[1], &[Rc::clone(&x0)]) == phi0_implies_phi0
@@ -954,6 +955,41 @@ fn test_wellformedness_instantiate() {
     assert!(instantiate(Rc::clone(&existsx0phi0), &[1], &[Rc::clone(&x0)]) == existsx0phi0);
     assert!(instantiate(Rc::clone(&muX0phi0), &[0], &[Rc::clone(&x0)]) == muX0x0);
     assert!(instantiate(Rc::clone(&muX0phi0), &[1], &[Rc::clone(&x0)]) == muX0phi0);
+
+    // Simultaneous instantiations
+    let phi1 = metavar_unconstrained(1);
+    let muX0phi1 = mu(0, Rc::clone(&phi1));
+    let muX0X0 = mu(0, Rc::clone(&X0));
+    assert!(instantiate(Rc::clone(&existsx0phi0), &[1, 2], &[Rc::clone(&x0), Rc::clone(&X0)]) == existsx0phi0);
+    assert!(instantiate(Rc::clone(&existsx0phi0), &[2, 1], &[Rc::clone(&x0), Rc::clone(&X0)]) == existsx0phi0);
+    assert!(instantiate(Rc::clone(&muX0phi0), &[1, 2], &[Rc::clone(&x0), Rc::clone(&X0)]) == muX0phi0);
+    assert!(instantiate(Rc::clone(&muX0phi0), &[2, 1], &[Rc::clone(&x0), Rc::clone(&X0)]) == muX0phi0);
+
+    assert!(instantiate(Rc::clone(&existsx0phi0), &[1, 0], &[Rc::clone(&x0), Rc::clone(&X0)]) == existsx0X0);
+    assert!(instantiate(Rc::clone(&existsx0phi0), &[0, 1], &[Rc::clone(&x0), Rc::clone(&X0)]) == existsx0x0);
+    assert!(instantiate(Rc::clone(&muX0phi0), &[1, 0], &[Rc::clone(&x0), Rc::clone(&X0)]) == muX0X0);
+    assert!(instantiate(Rc::clone(&muX0phi0), &[0, 1], &[Rc::clone(&x0), Rc::clone(&X0)]) == muX0x0);
+
+    let muX0phi0_implies_ph1 = implies(Rc::clone(&muX0phi0), Rc::clone(&phi1));
+    let muX0x0_implies_X0 = implies(Rc::clone(&muX0x0), Rc::clone(&X0));
+    assert!(instantiate(Rc::clone(&muX0phi0_implies_ph1), &[0, 1], &[Rc::clone(&x0), Rc::clone(&X0)]) == muX0x0_implies_X0);
+    assert!(instantiate(Rc::clone(&muX0phi0_implies_ph1), &[1, 0], &[Rc::clone(&X0), Rc::clone(&x0)]) == muX0x0_implies_X0);
+
+    let muX0phi0_app_ph1 = app(Rc::clone(&muX0phi0), Rc::clone(&phi1));
+    let muX0x0_app_X0 = app(Rc::clone(&muX0x0), Rc::clone(&X0));
+    assert!(instantiate(Rc::clone(&muX0phi0_app_ph1), &[0, 1], &[Rc::clone(&x0), Rc::clone(&X0)]) == muX0x0_app_X0);
+    assert!(instantiate(Rc::clone(&muX0phi0_app_ph1), &[1, 0], &[Rc::clone(&X0), Rc::clone(&x0)]) == muX0x0_app_X0);
+
+    let muX0ph1_implies_X0 = implies(Rc::clone(&muX0phi1), Rc::clone(&X0));
+    assert!(instantiate(Rc::clone(&muX0phi0_implies_ph1), &[0, 1], &[Rc::clone(&phi1), Rc::clone(&X0)]) == muX0ph1_implies_X0);
+    assert!(instantiate(Rc::clone(&muX0phi0_implies_ph1), &[1, 0], &[Rc::clone(&X0), Rc::clone(&phi1)]) == muX0ph1_implies_X0);
+
+    let muX0ph1_app_X0 = app(Rc::clone(&muX0phi1), Rc::clone(&X0));
+    assert!(instantiate(Rc::clone(&muX0phi0_app_ph1), &[0, 1], &[Rc::clone(&phi1), Rc::clone(&X0)]) == muX0ph1_app_X0);
+    assert!(instantiate(Rc::clone(&muX0phi0_app_ph1), &[1, 0], &[Rc::clone(&X0), Rc::clone(&phi1)]) == muX0ph1_app_X0);
+
+    assert!(instantiate(Rc::clone(&muX0phi0_app_ph1), &[0, 1, 1], &[Rc::clone(&phi1), Rc::clone(&X0), Rc::clone(&x0)]) == muX0ph1_app_X0);
+    assert!(instantiate(Rc::clone(&muX0phi0_app_ph1), &[1, 0, 0], &[Rc::clone(&X0), Rc::clone(&phi1), Rc::clone(&x0)]) == muX0ph1_app_X0);
 }
 
 #[test]
