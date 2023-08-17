95fecb7c7adc6a78b7f77965ae67db7148e263ea
Adam Fiedler <info@fiedlr.sk>
2023-08-17 17:06:48 +0200

    merge 2

--- generation/src/proof_generation/proofs/propositional.py
+++ generation/src/proof_generation/proofs/propositional.py
@@ -18,13 +18,13 @@ class Propositional(ProofExp):
         phi0 = MetaVar(0)
         bot = Mu(SVar(0), SVar(0))
         top = Implication(bot, bot)
-        return [Implication(phi0, phi0), top]
+        return [Implication(phi0, phi0), top, Implication(bot, phi0)]
 
     def claim_expressions(self) -> list[PatternExpression]:
-        return [self.phi0_implies_phi0, self.top]
+        return [self.phi0_implies_phi0, self.top, self.bot_implies_phi0]
 
     def proof_expressions(self) -> list[ProvedExpression]:
-        return [self.imp_reflexivity, self.top_intro]
+        return [self.imp_reflexivity, self.top_intro, self.bot_elim]
 
     # Notation
     # ========
@@ -56,6 +56,11 @@ class Propositional(ProofExp):
         if ret := self.load_notation('top'):
             return ret
         return self.save_notation('top', self.neg(self.bot))
+
+    def bot_implies_phi0(self) -> Pattern:
+        if ret := self.load_notation('bot-implies-phi0'):
+            return ret
+        return self.save_notation('bot-implies-phi0', self.implies(self.phi0(), self.bot()))
 
     # Proofs
     # ======
