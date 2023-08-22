3cdf43443b31b1ee0c67902ab36c7cfe7d71d142
Mihai <mihai.calancea@gmail.com>
2023-08-22 11:55:45 +0300

    Refactor mains.

--- checker/src/main.rs
+++ checker/src/main.rs
@@ -3,28 +3,21 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::Read;
 
-fn file_to_next(arg_index: usize, handler: impl FnOnce() -> String) -> impl FnMut() -> Option<u8> {
-    let path = std::env::args().nth(arg_index).unwrap_or_else(|| handler());
-    let mut obj = BufReader::new(File::open(path).unwrap()).bytes();
-    move || obj.next()?.ok()
-}
-
 pub fn main() {
-    let proof_index;
-    let claim_index;
+    let (mut proof_reader, mut claims_reader) = match std::env::args().len() {
+        2 => (
+            BufReader::new(File::open(std::env::args().nth(1).unwrap()).unwrap()).bytes(),
+            BufReader::new(File::open("/dev/null").unwrap()).bytes(),
+        ),
+        3 => (
+            BufReader::new(File::open(std::env::args().nth(2).unwrap()).unwrap()).bytes(),
+            BufReader::new(File::open(std::env::args().nth(1).unwrap()).unwrap()).bytes(),
+        ),
+        other => panic!("Expected 1 or 2 arguments. Received {}.", other),
+    };
 
-    if std::env::args().len() >= 3 {
-        // If both claim and proof are present, the claim comes first
-        proof_index = 2;
-        claim_index = 1
-    } else {
-        // Otherwise, the single non-trivial argument is the proof
-        proof_index = 1;
-        claim_index = 2;
-    }
+    let proof_next = &mut (|| proof_reader.next()?.ok());
+    let claims_next = &mut (|| claims_reader.next()?.ok());
 
-    let proof_next = &mut (file_to_next(proof_index, || panic!("No proof file path given")));
-    let claim_next = &mut (file_to_next(claim_index, || return "/dev/null".to_string()));
-
-    verify(proof_next, claim_next);
+    verify(proof_next, claims_next);
 }
--- risc0/src/main.rs
+++ risc0/src/main.rs
@@ -8,34 +8,20 @@ use std::time::Instant;
 
 fn main() {
     let now = Instant::now();
-    let empty_file = "/dev/null".to_string();
-
-    let proof_index;
-    let claim_index;
-
-    if std::env::args().len() >= 3 {
-        // If both claim and proof are present, the claim comes first
-        proof_index = 2;
-        claim_index = 1
-    } else {
-        // Otherwise, the single non-trivial argument is the proof
-        proof_index = 1;
-        claim_index = 2;
-    }
 
     println!("Setting up env...");
 
-    let proof_filepath = std::env::args()
-        .nth(proof_index)
-        .expect("No proof file path given");
-    let proof_file = File::open(proof_filepath).expect("The proof file was not found");
-    let proof_reader = BufReader::new(proof_file);
+    if std::env::args().len() != 3 {
+        panic!("Expected 2 arguments. Received {}.", std::env::args().len())
+    }
 
-    let claims_filepath = std::env::args()
-        .nth(claim_index)
-        .unwrap_or(empty_file.clone());
+    let claims_filepath = std::env::args().nth(1).expect("No claim file path given");
     let claims_file = File::open(claims_filepath).expect("The claims file was not found");
     let claims_reader = BufReader::new(claims_file);
+
+    let proof_filepath = std::env::args().nth(2).expect("No proof file path given");
+    let proof_file = File::open(proof_filepath).expect("The proof file was not found");
+    let proof_reader = BufReader::new(proof_file);
 
     // First, we construct an executor environment
     let env = ExecutorEnv::builder()
