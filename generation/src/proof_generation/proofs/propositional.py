270ca51e226390af14b28f63fd6be250074df6af
Adam Fiedler <info@fiedlr.sk>
2023-09-04 01:03:20 +0200

    Make instantiations use static values (#114)
    This introduces taking static values as the values for instantiations,
    which is expected and is not currently working in the DSL. The gist of
    it is that we write:
    
    ```python
    proved.instantiate(1: EVar(1))`
    ```
    
    instead of
    
    ```python
    proved.instantiate(1: self.evar(1))
    ```
    
    The advantage is that it is much easier to use and the correct order of
    evaluation is handled for us, so it's also easy to extract subproofs and
    work with proof constructors.
    
    Disadvantage is that now the patterns are in memory and need to be
    "parsed" to create the expression, which might cause slightly worse
    performance in the generation phase.
    Also, notation is currently disabled, as we cannot use `.load` in such
    static expressions, which is why the proofs are longer. It would lead to
    "double-loading". This will be handled in future PRs, probably by
    introducing a special `Notation` subclass of `Pattern`.

--- generation/src/proof_generation/proofs/propositional.py
+++ generation/src/proof_generation/proofs/propositional.py
@@ -8,6 +8,14 @@ from proof_generation.proof import Implication, MetaVar, Mu, ProofExp, SVar
 if TYPE_CHECKING:
     from proof_generation.proof import BasicInterpreter, Pattern, PatternExpression, Proved, ProvedExpression
 
+bot = Mu(SVar(0), SVar(0))
+phi0 = MetaVar(0)
+phi0_implies_phi0 = Implication(phi0, phi0)
+
+
+def neg(p: Pattern) -> Pattern:
+    return Implication(p, bot)
+
 
 class Propositional(ProofExp):
     def __init__(self, interpreter: BasicInterpreter) -> None:
@@ -100,10 +108,10 @@ class Propositional(ProofExp):
         # fmt: off
         return self.modus_ponens(
             self.modus_ponens(
-                self.prop2().instantiate({1: self.phi0_implies_phi0(), 2: self.phi0()}),
-                self.prop1().instantiate({1: self.phi0_implies_phi0()}),
+                self.prop2().instantiate({1: phi0_implies_phi0, 2: phi0}),
+                self.prop1().instantiate({1: phi0_implies_phi0}),
             ),
-            self.prop1().instantiate({1: self.phi0()}),
+            self.prop1().instantiate({1: phi0}),
         )
 
     # phi1 -> phi2 and phi2 -> phi3 yields also a proof of phi1 -> phi3
@@ -123,51 +131,60 @@ class Propositional(ProofExp):
                 raise AssertionError('Expected implication')
 
         return self.modus_ponens(
+            # (phi0 -> phi1) -> (phi0 -> phi2)
             self.modus_ponens(
-                self.prop2().instantiate({1: phi1, 2: phi2, 0: self.metavar(1)}),
-                self.modus_ponens(self.prop1().instantiate({0: phi1_imp_phi2_conc}), phi1_imp_phi2),
+                # (1 -> (phi1 -> phi2)) -> ((1 -> phi1) -> (1 -> phi2))
+                self.prop2().instantiate({0: MetaVar(1), 1: phi1, 2: phi2}),
+                #  1 -> (phi1 -> phi2)
+                self.modus_ponens(
+                    # (phi1 -> phi2) -> (1 -> (phi1 -> phi2))
+                    self.prop1().instantiate({0: phi1_imp_phi2_conc}),
+                    phi1_imp_phi2,
+                ),
             ).instantiate({1: phi0}),
             phi0_imp_phi1,
         )
 
     def top_intro(self) -> Proved:
-        return self.imp_reflexivity().instantiate({0: self.bot()})
+        return self.imp_reflexivity().instantiate({0: bot})
 
     def bot_elim(self) -> Proved:
+        neg_neg_phi0 = neg(neg(phi0))
+
         return self.modus_ponens(
             # ((bot -> neg neg 0) -> (bot -> 0)))
             self.modus_ponens(
                 # (bot -> (neg neg 0 -> 0)) -> ((bot -> neg neg 0) -> (bot -> 0))
-                self.prop2().instantiate({0: self.bot(), 1: self.neg(self.neg_phi0), 2: self.phi0()}),
+                self.prop2().instantiate({0: bot, 1: neg_neg_phi0, 2: phi0}),
                 # (bot -> (neg neg 0 -> 0))
                 self.modus_ponens(
                     # (neg neg 0 -> 0) -> (bot -> (neg neg 0 -> 0))
-                    self.prop1().instantiate({0: self.implies(self.neg(self.neg_phi0), self.phi0()), 1: self.bot()}),
+                    self.prop1().instantiate({0: Implication(neg_neg_phi0, phi0), 1: bot}),
                     # (neg neg 0 -> 0)
-                    self.prop3().instantiate({0: self.phi0()}),
+                    self.prop3().instantiate({0: phi0}),
                 ),
             ),
             # (bot -> (neg neg phi0))
-            self.prop1().instantiate({0: self.bot(), 1: self.neg(self.phi0)}),
+            self.prop1().instantiate({0: bot, 1: neg(phi0)}),
         )
 
     # (neg phi0 -> bot) -> phi0
     def contradiction_proof(self) -> Proved:
-        return self.prop3().instantiate({0: self.phi0()})
+        return self.prop3().instantiate({0: phi0})
 
     # (((ph0 -> bot) -> ph0) -> ph0)
     def peirce_bot(self) -> Proved:
         def phi0_bot_imp_ph0() -> Pattern:
             # ((ph0 -> bot) -> ph0)
-            return self.implies(self.implies(self.phi0(), self.bot()), self.phi0())
+            return Implication(Implication(phi0, bot), phi0)
 
         def phi0_bot_imp_bot() -> Pattern:
             # (ph0 -> bot) -> bot)
-            return self.implies(self.implies(self.phi0(), self.bot()), self.bot())
+            return Implication(Implication(phi0, bot), bot)
 
         def phi0_bot_imp_phi0_bot() -> Pattern:
             # (phi0 -> bot) -> (phi0->bot)
-            return self.implies(self.implies(self.phi0(), self.bot()), self.implies(self.phi0(), self.bot()))
+            return Implication(Implication(phi0, bot), Implication(phi0, bot))
 
         def modus_ponens_1() -> Proved:
             return self.modus_ponens(
@@ -177,20 +194,20 @@ class Propositional(ProofExp):
                         0: phi0_bot_imp_ph0(),
                         # ((ph0 -> bot) -> bot) = neg 0 -> bot
                         1: phi0_bot_imp_bot(),
-                        2: self.phi0(),
+                        2: phi0,
                     }
                 ),
                 self.modus_ponens(
                     self.prop1().instantiate(
                         {
                             # (((ph0 -> bot) -> bot) -> ph0)
-                            0: self.implies(phi0_bot_imp_bot(), self.phi0()),
+                            0: Implication(phi0_bot_imp_bot(), phi0),
                             # ((ph0 -> bot) -> ph0)
                             1: phi0_bot_imp_ph0(),
                         }
                     ),
                     # ph0
-                    self.prop3().instantiate({0: self.phi0()}),
+                    self.prop3().instantiate({0: phi0}),
                 ),
             )
 
@@ -201,19 +218,19 @@ class Propositional(ProofExp):
                         0: phi0_bot_imp_ph0(),
                         1: phi0_bot_imp_phi0_bot(),
                         # (((ph0 -> bot) -> phi0) -> ((ph0 -> bot) -> bot)))
-                        2: self.implies(phi0_bot_imp_ph0(), phi0_bot_imp_bot()),
+                        2: Implication(phi0_bot_imp_ph0(), phi0_bot_imp_bot()),
                     }
                 ),
                 self.modus_ponens(
                     self.prop1().instantiate(
                         {
                             # ((phi0 -> bot) -> (phi0 -> bot)) -> (((ph0 -> bot) -> phi0) -> ((ph0 -> bot) -> bot))),
-                            0: self.implies(
-                                self.implies(
-                                    self.implies(self.phi0(), self.bot()),
-                                    self.implies(self.phi0(), self.bot()),
+                            0: Implication(
+                                Implication(
+                                    Implication(phi0, bot),
+                                    Implication(phi0, bot),
                                 ),
-                                self.implies(
+                                Implication(
                                     phi0_bot_imp_ph0(),
                                     phi0_bot_imp_bot(),
                                 ),
@@ -223,9 +240,9 @@ class Propositional(ProofExp):
                     ),
                     self.prop2().instantiate(
                         {
-                            0: self.implies(self.phi0(), self.bot()),
-                            1: self.phi0(),
-                            2: self.bot(),
+                            0: Implication(phi0, bot),
+                            1: phi0,
+                            2: bot,
                         }
                     ),
                 ),
@@ -244,7 +261,7 @@ class Propositional(ProofExp):
                 self.imp_reflexivity().instantiate(
                     {
                         # (phi0 -> bot)
-                        0: self.implies(self.phi0(), self.bot()),
+                        0: Implication(phi0, bot),
                     }
                 ),
             )
