bed52a280e6c91cfeab28863cab378a423a2667a
Ilja Zakharov <ilja.zakharov@runtimeverification.com>
2023-09-08 13:27:00 +0200

    Add dictionary that allows to check as keys both Metavariables and their names

--- generation/src/mm_transfer/converter/vardict.py
+++ generation/src/mm_transfer/converter/vardict.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 from collections import UserDict
 from typing import Any
 
@@ -6,8 +7,12 @@ from mm_transfer.metamath.ast import Metavariable
 
 
 class Vardict(UserDict):
-
-    def __init__(self, __data: dict[str, Any] | None = None, expected: type = Any) -> None:
+    """
+    This dictionary simplifies checking that a Metavariable is in the dictionary.
+    You can type just: "metavar in varidct" in both cases where metavar is a Metavariable
+    and where it is a string. Same is for saving new items.
+    """
+    def __init__(self, __data: dict[str, Any] | None = None, expected: type | None = None) -> None:
         super().__init__(__data)
         self._expected = expected
 
@@ -19,6 +24,6 @@ class Vardict(UserDict):
     def __setitem__(self, __key: Metavariable | str, __value: Any) -> None:
         if isinstance(__key, Metavariable):
             __key = __key.name
-        if not isinstance(__value, self._expected):
+        if isinstance(self._expected, type) and not isinstance(__value, self._expected):
             raise TypeError(f'Expected {self._expected}, got {type(__value).__name__}')
         return super().__setitem__(__key, __value)
