72d78cb3c288ec8472c8a78981abefb76b9340a0
Ilja Zakharov <ilja.zakharov@runtimeverification.com>
2023-09-12 15:54:27 +0200

    Add the representation of a Notation with a buildin type check for its arguments

---
+++ generation/src/mm_transfer/converter/representation.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+from mypy_extensions import VarArg
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    import proof_generation.pattern as nf
+
+
+@dataclass(frozen=True)
+class Notation:
+    name: str
+    type_check: Callable[[VarArg(nf.Pattern)], bool]
+    callable: Callable[[VarArg(nf.Pattern)], nf.Pattern]
+
+    def __call__(self, *args: nf.Pattern) -> nf.Pattern:
+        assert self.type_check(*args), f'Invalid arguments for {self.name}'
+        return self.callable(*args)
