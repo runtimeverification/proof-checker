7c7f492c24cf1d63edba80cc91468f0d66b0cd5b
MirceaS <mirceasebe@yahoo.com>
2023-09-27 16:17:17 +0300

    adjusted initial capacity of vectors used for verification

--- checker/src/lib.rs
+++ checker/src/lib.rs
@@ -944,10 +944,10 @@ pub fn verify<'a>(
     proof_next_byte: &mut impl FnMut() -> Option<InstByte>,
 ) {
     let mut claims: Claims = vec![];
-    let mut axioms: Memory = vec![];
+    let mut axioms: Memory = Vec::with_capacity(128);
     execute_instructions(
         gamma_next_byte,
-        &mut vec![], // stack is empty initially.
+        &mut Vec::with_capacity(32), // stack is empty initially.
         &mut vec![], // memory is empty initially.
         &mut vec![], // claims is unused in this phase.
         &mut axioms, // populate axioms
@@ -955,7 +955,7 @@ pub fn verify<'a>(
     );
     execute_instructions(
         claims_next_byte,
-        &mut vec![], // stack is empty initially.
+        &mut Vec::with_capacity(32), // stack is empty initially.
         &mut vec![], // memory is empty initially, though we may think of reusing for sharing notation between phases.
         &mut claims, // claims populated in this phase
         &mut vec![], // axioms is unused in this phase.
@@ -963,7 +963,7 @@ pub fn verify<'a>(
     );
     execute_instructions(
         proof_next_byte,
-        &mut vec![], // stack is empty initially.
+        &mut Vec::with_capacity(32), // stack is empty initially.
         &mut axioms, // axioms are used as initial memory
         &mut claims, // claims are consumed by publish instruction
         &mut vec![], // axioms is unused in this phase.
