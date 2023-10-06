c5a3ce8860600fa02bdb298c66a12a0fbe39b727
Ilja Zakharov <ilja.zakharov@runtimeverification.com>
2023-10-06 15:19:40 +0200

    Rename definition generating script

--- generation/src/kore_transfer/generate_definition.py
+++ generation/src/kore_transfer/generate_definition.py
@@ -3,7 +3,8 @@ import argparse
 from pyk.ktool.kompile import kompile
 
 
-def main(k_file: str, program: str, output_dir: str) -> None:
+def main(k_file: str, output_dir: str) -> None:
+    print(f'Kompiling target {k_file} to {output_dir}')
     kompile(main_file=k_file, backend='llvm', output_dir=output_dir)
     return
 
@@ -18,4 +19,4 @@ if __name__ == '__main__':
     argparser.add_argument('output_dir', type=str)
     args = argparser.parse_args()
 
-    main(args.k_file, args.program, args.output_dir)
+    main(args.k_file, args.output_dir)
