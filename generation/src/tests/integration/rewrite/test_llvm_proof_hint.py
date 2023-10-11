6c5acb829540c5a046731df59b207abaf3da8d78
Musab Alturki <musab.alturki@runtimeverification.com>
2023-10-11 14:24:05 -0500

    Add single-rewrite proof hints test

--- generation/src/tests/integration/rewrite/test_llvm_proof_hint.py
+++ generation/src/tests/integration/rewrite/test_llvm_proof_hint.py
@@ -10,3 +10,7 @@ def test_parse_proof_hint() -> None:
     hint = LLVMRewriteTrace.parse(bin_hint)
     assert len(hint.trace) == 64
     # TODO: Strenghten these assertions.
+
+    bin_hint = Path('proof-hints/single-rewrite_proof-hints.bin').read_bytes()
+    hint = LLVMRewriteTrace.parse(bin_hint)
+    assert len(hint.trace) == 1
