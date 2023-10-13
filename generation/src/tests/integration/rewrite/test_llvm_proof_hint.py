dbfd172c50e691072bb0571deafa04490fffce7c
musab.alturki <musab.alturki@gmail.com>
2023-10-13 11:45:16 -0500

    Enable testing all supported proof hints

--- generation/src/tests/integration/rewrite/test_llvm_proof_hint.py
+++ generation/src/tests/integration/rewrite/test_llvm_proof_hint.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import pyk.kllvm.load  # noqa: F401
@@ -5,12 +6,38 @@ import pyk.kllvm.load  # noqa: F401
 from rewrite.llvm_proof_hint import LLVMRewriteTrace
 
 
-def test_parse_proof_hint() -> None:
-    bin_hint = Path('proof-hints/proof-hints.bin').read_bytes()
-    hint = LLVMRewriteTrace.parse(bin_hint)
-    assert len(hint.trace) == 64
-    # TODO: Strenghten these assertions.
+def list_files_recursively(directory: str) -> list[str]:
+    file_paths = []
 
-    bin_hint = Path('proof-hints/single-rewrite/foo-a.single-rewrite.hints').read_bytes()
-    hint = LLVMRewriteTrace.parse(bin_hint)
-    assert len(hint.trace) == 1
+    for root, _, files in os.walk(directory):
+        for file in files:
+            file_paths.append(os.path.join(root, file))
+
+    return file_paths
+
+
+def test_parse_proof_hint() -> None:
+    hints_dir_path = 'proof-hints'
+    assert os.path.exists(
+        hints_dir_path
+    ), 'The proof hints directory {!r} does not exist. Make sure proof hints are generated and stored in this directory before running the integration tests.'.format(
+        hints_dir_path
+    )
+
+    hint_files = list_files_recursively(hints_dir_path)
+    for file_path in hint_files:
+        hint_name = os.path.splitext(os.path.basename(file_path))[0]
+        hint_bin = Path(file_path).read_bytes()
+        hint = LLVMRewriteTrace.parse(hint_bin)
+
+        # Add per-test logic here
+        if hint_name == '0_rewrites.trivial':
+            assert len(hint.trace) == 0
+        elif hint_name == '1_rewrite.trivial':
+            assert len(hint.trace) == 1
+        elif hint_name == '2_rewrites.trivial':
+            assert len(hint.trace) == 2
+        elif hint_name == 'foo-a.single-rewrite':
+            assert len(hint.trace) == 1
+        elif hint_name == 'mul_3_5.peano':
+            assert len(hint.trace) == 58
