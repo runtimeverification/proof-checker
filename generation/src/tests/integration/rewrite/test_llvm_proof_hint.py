57dbc6a910862fc648e0a4c7599fd6d7f03f52e3
musab.alturki <musab.alturki@gmail.com>
2023-10-14 01:46:11 -0500

    Expand on the single rewrite test

--- generation/src/tests/integration/rewrite/test_llvm_proof_hint.py
+++ generation/src/tests/integration/rewrite/test_llvm_proof_hint.py
@@ -30,14 +30,34 @@ def test_parse_proof_hint() -> None:
         hint_bin = Path(file_path).read_bytes()
         hint = LLVMRewriteTrace.parse(hint_bin)
 
-        # Add per-test logic here
+        # Add example-specific testing logic here
         if hint_name == '0_rewrites.trivial':
             assert len(hint.trace) == 0
+
         elif hint_name == '1_rewrite.trivial':
             assert len(hint.trace) == 1
+
         elif hint_name == '2_rewrites.trivial':
             assert len(hint.trace) == 2
+
         elif hint_name == 'foo-a.single-rewrite':
+            # A single rewrite step
             assert len(hint.trace) == 1
+
+            # Contents of the k cell in the initial configuration
+            p = hint.initial_config.patterns[0].args[0]  # type: ignore
+            assert p.symbol == 'kseq'
+            assert p.args[0].args[0].symbol == "LblFooA'LParRParUnds'SINGLE-REWRITE-SYNTAX'Unds'Foo"
+            assert p.args[1].symbol == 'dotk'
+
+            # Rule applied in the single rewrite step
+            assert hint.trace[0].rule_ordinal == 92
+
+            # Contents of the k cell in the final configuration
+            p = hint.trace[0].post_config.patterns[0].args[0]  # type: ignore
+            assert p.symbol == 'kseq'
+            assert p.args[0].args[0].symbol == "LblFooB'LParRParUnds'SINGLE-REWRITE-SYNTAX'Unds'Foo"
+            assert p.args[1].symbol == 'dotk'
+
         elif hint_name == 'mul_3_5.peano':
             assert len(hint.trace) == 58
