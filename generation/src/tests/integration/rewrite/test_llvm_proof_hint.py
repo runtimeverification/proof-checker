952963182e15f564b7ed659e8722e4593da28b37
Musab Alturki <42160792+malturki@users.noreply.github.com>
2023-10-16 12:29:43 -0500

    Enable testing all supported proof hints and expand on the `single-rewrite` test (#287)
    Closes #278

--- generation/src/tests/integration/rewrite/test_llvm_proof_hint.py
+++ generation/src/tests/integration/rewrite/test_llvm_proof_hint.py
@@ -1,16 +1,49 @@
+import os
 from pathlib import Path
 
 import pyk.kllvm.load  # noqa: F401
 
 from rewrite.llvm_proof_hint import LLVMRewriteTrace
 
+HINTS_DIR_PATH = 'proof-hints'
 
-def test_parse_proof_hint() -> None:
-    bin_hint = Path('proof-hints/proof-hints.bin').read_bytes()
-    hint = LLVMRewriteTrace.parse(bin_hint)
-    assert len(hint.trace) == 64
-    # TODO: Strenghten these assertions.
 
-    bin_hint = Path('proof-hints/single-rewrite/foo-a.single-rewrite.hints').read_bytes()
-    hint = LLVMRewriteTrace.parse(bin_hint)
+def read_proof_hint(filepath: str) -> LLVMRewriteTrace:
+    hint_bin = Path(os.path.join(HINTS_DIR_PATH, filepath)).read_bytes()
+    return LLVMRewriteTrace.parse(hint_bin)
+
+
+def test_parse_proof_hint_single_rewrite() -> None:
+    hint = read_proof_hint('single-rewrite/foo-a.single-rewrite.hints')
+
+    # A single rewrite step
     assert len(hint.trace) == 1
+
+    # Contents of the k cell in the initial configuration
+    k_cell = hint.initial_config.patterns[0].dict['args'][0]
+    assert k_cell['name'] == 'kseq'
+    assert k_cell['args'][0]['args'][0]['name'] == "LblFooA'LParRParUnds'SINGLE-REWRITE-SYNTAX'Unds'Foo"
+    assert k_cell['args'][1]['name'] == 'dotk'
+
+    # Rule applied in the single rewrite step
+    assert hint.trace[0].rule_ordinal == 92
+
+    # Contents of the k cell in the final configuration
+    k_cell = hint.trace[0].post_config.patterns[0].dict['args'][0]
+    assert k_cell['name'] == 'kseq'
+    assert k_cell['args'][0]['args'][0]['name'] == "LblFooB'LParRParUnds'SINGLE-REWRITE-SYNTAX'Unds'Foo"
+    assert k_cell['args'][1]['name'] == 'dotk'
+
+
+def test_parse_proof_hint_trivial() -> None:
+    names = ['0_rewrites.trivial', '1_rewrite.trivial', '2_rewrites.trivial']
+    hints = [read_proof_hint('trivial/' + name + '.hints') for name in names]
+
+    for i in range(len(hints)):
+        assert len(hints[i].trace) == i
+
+
+def test_parse_proof_hint_peano() -> None:
+    hint = read_proof_hint('peano/mul_3_5.peano.hints')
+
+    assert len(hint.trace) == 58
