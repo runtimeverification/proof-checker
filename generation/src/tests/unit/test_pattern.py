09f1ffce31ceebb94a8eb105c5dbb355c49c5819
Nishant Rodrigues <nishantjr@gmail.com>
2023-10-20 13:05:03 -0400

    test_pattern: Extract variables

--- generation/src/tests/unit/test_pattern.py
+++ generation/src/tests/unit/test_pattern.py
@@ -11,43 +11,49 @@ if TYPE_CHECKING:
 
     from proof_generation.pattern import Pattern
 
+phi0 = MetaVar(0)
+phi1 = MetaVar(1)
+sigma0 = Symbol('s0')
+sigma1 = Symbol('s1')
+sigma2 = Symbol('s2')
+
 
 @pytest.mark.parametrize(
     'pattern, evar_id, plug, expected',
     [
         # Atomic cases
-        [EVar(0), 0, Symbol('s1'), Symbol('s1')],
+        [EVar(0), 0, sigma1, sigma1],
         [EVar(0), 0, EVar(2), EVar(2)],
         [EVar(0), 1, EVar(2), EVar(0)],
-        [SVar(0), 0, Symbol('s0'), SVar(0)],
+        [SVar(0), 0, sigma0, SVar(0)],
         [SVar(1), 0, EVar(0), SVar(1)],
-        [Symbol('s0'), 0, Symbol('s1'), Symbol('s0')],
+        [sigma0, 0, sigma1, sigma0],
         # Distribute over subpatterns
-        [Implies(EVar(7), Symbol('s1')), 7, Symbol('s0'), Implies(Symbol('s0'), Symbol('s1'))],
-        [Implies(EVar(7), Symbol('s1')), 6, Symbol('s0'), Implies(EVar(7), Symbol('s1'))],
-        [App(EVar(7), Symbol('s1')), 7, Symbol('s0'), App(Symbol('s0'), Symbol('s1'))],
-        [App(EVar(7), Symbol('s1')), 6, Symbol('s0'), App(EVar(7), Symbol('s1'))],
+        [Implies(EVar(7), sigma1), 7, sigma0, Implies(sigma0, sigma1)],
+        [Implies(EVar(7), sigma1), 6, sigma0, Implies(EVar(7), sigma1)],
+        [App(EVar(7), sigma1), 7, sigma0, App(sigma0, sigma1)],
+        [App(EVar(7), sigma1), 6, sigma0, App(EVar(7), sigma1)],
         # Distribute over subpatterns unless evar_id = binder
-        [Exists(1, EVar(1)), 1, Symbol('s2'), Exists(1, EVar(1))],
-        [Exists(0, EVar(1)), 1, Symbol('s2'), Exists(0, Symbol('s2'))],
-        [Mu(1, EVar(1)), 0, Symbol('s2'), Mu(1, EVar(1))],
-        [Mu(1, EVar(1)), 1, Symbol('s2'), Mu(1, Symbol('s2'))],
+        [Exists(1, EVar(1)), 1, sigma2, Exists(1, EVar(1))],
+        [Exists(0, EVar(1)), 1, sigma2, Exists(0, sigma2)],
+        [Mu(1, EVar(1)), 0, sigma2, Mu(1, EVar(1))],
+        [Mu(1, EVar(1)), 1, sigma2, Mu(1, sigma2)],
         # Subst on metavar should wrap in constructor
-        [MetaVar(0), 0, Symbol('s1'), ESubst(MetaVar(0), EVar(0), Symbol('s1'))],
+        [phi0, 0, sigma1, ESubst(phi0, EVar(0), sigma1)],
         # Subst when evar_id is fresh should do nothing
-        [MetaVar(0, e_fresh=(EVar(0), EVar(1))), 0, Symbol('s1'), MetaVar(0, e_fresh=(EVar(0), EVar(1)))],
+        [MetaVar(0, e_fresh=(EVar(0), EVar(1))), 0, sigma1, MetaVar(0, e_fresh=(EVar(0), EVar(1)))],
         # Subst on substs should stack
         [
-            ESubst(MetaVar(0), EVar(0), Symbol('s1')),
+            ESubst(phi0, EVar(0), sigma1),
             0,
-            Symbol('s1'),
-            ESubst(ESubst(MetaVar(0), EVar(0), Symbol('s1')), EVar(0), Symbol('s1')),
+            sigma1,
+            ESubst(ESubst(phi0, EVar(0), sigma1), EVar(0), sigma1),
         ],
         [
-            SSubst(MetaVar(0), SVar(0), Symbol('s1')),
+            SSubst(phi0, SVar(0), sigma1),
             0,
-            Symbol('s1'),
-            ESubst(SSubst(MetaVar(0), SVar(0), Symbol('s1')), EVar(0), Symbol('s1')),
+            sigma1,
+            ESubst(SSubst(phi0, SVar(0), sigma1), EVar(0), sigma1),
         ],
     ],
 )
@@ -68,14 +74,14 @@ stack_mixed1 = lambda term: ESubst(SSubst(pattern=term, var=SVar(1), plug=EVar(1
 @pytest.mark.parametrize(
     'stack, mvar, plugs, expected',
     [
-        [esubst_stack_seq, MetaVar(0), {0: EVar(0)}, EVar(2)],
-        [esubst_stack_seq, MetaVar(0), {0: EVar(1)}, EVar(2)],
-        [esubst_stack_seq, MetaVar(1), {0: EVar(1)}, esubst_stack_seq(MetaVar(1))],
-        [esubst_stack_seq, MetaVar(0), {0: MetaVar(1)}, esubst_stack_seq(MetaVar(1))],
-        [esubst_stack_seq_rev, MetaVar(0), {0: EVar(0)}, EVar(1)],
-        [esubst_stack_seq_rev, MetaVar(0), {0: MetaVar(1)}, esubst_stack_seq_rev(MetaVar(1))],
-        [stack_mixed1, MetaVar(0), {0: SVar(1)}, EVar(2)],
-        [stack_mixed1, MetaVar(0), {0: SVar(2)}, SVar(2)],
+        [esubst_stack_seq, phi0, {0: EVar(0)}, EVar(2)],
+        [esubst_stack_seq, phi0, {0: EVar(1)}, EVar(2)],
+        [esubst_stack_seq, phi1, {0: EVar(1)}, esubst_stack_seq(phi1)],
+        [esubst_stack_seq, phi0, {0: phi1}, esubst_stack_seq(phi1)],
+        [esubst_stack_seq_rev, phi0, {0: EVar(0)}, EVar(1)],
+        [esubst_stack_seq_rev, phi0, {0: phi1}, esubst_stack_seq_rev(phi1)],
+        [stack_mixed1, phi0, {0: SVar(1)}, EVar(2)],
+        [stack_mixed1, phi0, {0: SVar(2)}, SVar(2)],
     ],
 )
 def test_instantiate_esubst(
@@ -88,38 +94,38 @@ def test_instantiate_esubst(
     'pattern, svar_id, plug, expected',
     [
         # Atomic cases
-        [EVar(0), 0, Symbol('s1'), EVar(0)],
+        [EVar(0), 0, sigma1, EVar(0)],
         [EVar(0), 1, EVar(2), EVar(0)],
-        [SVar(0), 0, Symbol('s0'), Symbol('s0')],
+        [SVar(0), 0, sigma0, sigma0],
         [SVar(1), 0, EVar(0), SVar(1)],
-        [Symbol('s0'), 0, Symbol('s1'), Symbol('s0')],
+        [sigma0, 0, sigma1, sigma0],
         # Distribute over subpatterns
-        [Implies(SVar(7), Symbol('s1')), 7, Symbol('s0'), Implies(Symbol('s0'), Symbol('s1'))],
-        [Implies(SVar(7), Symbol('s1')), 6, Symbol('s0'), Implies(SVar(7), Symbol('s1'))],
-        [App(SVar(7), Symbol('s1')), 7, Symbol('s0'), App(Symbol('s0'), Symbol('s1'))],
-        [App(SVar(7), Symbol('s1')), 6, Symbol('s0'), App(SVar(7), Symbol('s1'))],
+        [Implies(SVar(7), sigma1), 7, sigma0, Implies(sigma0, sigma1)],
+        [Implies(SVar(7), sigma1), 6, sigma0, Implies(SVar(7), sigma1)],
+        [App(SVar(7), sigma1), 7, sigma0, App(sigma0, sigma1)],
+        [App(SVar(7), sigma1), 6, sigma0, App(SVar(7), sigma1)],
         # Distribute over subpatterns unless svar_id = binder
-        [Exists(1, SVar(0)), 0, Symbol('s2'), Exists(1, Symbol('s2'))],
-        [Exists(1, Symbol('s1')), 1, Symbol('s2'), Exists(1, Symbol('s1'))],
-        [Mu(1, SVar(1)), 0, Symbol('s2'), Mu(1, SVar(1))],
-        [Mu(1, SVar(1)), 1, Symbol('s2'), Mu(1, SVar(1))],
-        [Mu(1, SVar(2)), 2, Symbol('s2'), Mu(1, Symbol('s2'))],
+        [Exists(1, SVar(0)), 0, sigma2, Exists(1, sigma2)],
+        [Exists(1, sigma1), 1, sigma2, Exists(1, sigma1)],
+        [Mu(1, SVar(1)), 0, sigma2, Mu(1, SVar(1))],
+        [Mu(1, SVar(1)), 1, sigma2, Mu(1, SVar(1))],
+        [Mu(1, SVar(2)), 2, sigma2, Mu(1, sigma2)],
         # Subst on metavar should wrap in constructor
-        [MetaVar(0), 0, Symbol('s1'), SSubst(MetaVar(0), SVar(0), Symbol('s1'))],
+        [phi0, 0, sigma1, SSubst(phi0, SVar(0), sigma1)],
         # Subst when evar_id is fresh should do nothing
-        [MetaVar(0, s_fresh=(SVar(0), SVar(1))), 0, Symbol('s1'), MetaVar(0, s_fresh=(SVar(0), SVar(1)))],
+        [MetaVar(0, s_fresh=(SVar(0), SVar(1))), 0, sigma1, MetaVar(0, s_fresh=(SVar(0), SVar(1)))],
         # Subst on substs should stack
         [
-            ESubst(MetaVar(0), EVar(0), Symbol('s1')),
+            ESubst(phi0, EVar(0), sigma1),
             0,
-            Symbol('s1'),
-            SSubst(ESubst(MetaVar(0), EVar(0), Symbol('s1')), SVar(0), Symbol('s1')),
+            sigma1,
+            SSubst(ESubst(phi0, EVar(0), sigma1), SVar(0), sigma1),
         ],
         [
-            SSubst(MetaVar(0), SVar(0), Symbol('s1')),
+            SSubst(phi0, SVar(0), sigma1),
             0,
-            Symbol('s1'),
-            SSubst(SSubst(MetaVar(0), SVar(0), Symbol('s1')), SVar(0), Symbol('s1')),
+            sigma1,
+            SSubst(SSubst(phi0, SVar(0), sigma1), SVar(0), sigma1),
         ],
     ],
 )
@@ -137,16 +143,16 @@ ssubst_stack_seq_rev = lambda term: SSubst(SSubst(pattern=term, var=SVar(1), plu
 @pytest.mark.parametrize(
     'stack, mvar, plugs, expected',
     [
-        [ssubst_stack_seq, MetaVar(0), {0: SVar(0)}, SVar(2)],
-        [ssubst_stack_seq, MetaVar(0), {0: SVar(1)}, SVar(2)],
-        [ssubst_stack_seq, MetaVar(1), {0: SVar(1)}, ssubst_stack_seq(MetaVar(1))],
-        [ssubst_stack_seq, MetaVar(0), {0: MetaVar(1)}, ssubst_stack_seq(MetaVar(1))],
-        [ssubst_stack_seq_rev, MetaVar(0), {0: SVar(0)}, SVar(1)],
-        [ssubst_stack_seq_rev, MetaVar(0), {0: MetaVar(1)}, ssubst_stack_seq_rev(MetaVar(1))],
-        [stack_mixed1, MetaVar(0), {0: SVar(1)}, EVar(2)],
-        [stack_mixed1, MetaVar(0), {0: SVar(2)}, SVar(2)],
+        [ssubst_stack_seq, phi0, {0: SVar(0)}, SVar(2)],
+        [ssubst_stack_seq, phi0, {0: SVar(1)}, SVar(2)],
+        [ssubst_stack_seq, phi1, {0: SVar(1)}, ssubst_stack_seq(phi1)],
+        [ssubst_stack_seq, phi0, {0: phi1}, ssubst_stack_seq(phi1)],
+        [ssubst_stack_seq_rev, phi0, {0: SVar(0)}, SVar(1)],
+        [ssubst_stack_seq_rev, phi0, {0: phi1}, ssubst_stack_seq_rev(phi1)],
+        [stack_mixed1, phi0, {0: SVar(1)}, EVar(2)],
+        [stack_mixed1, phi0, {0: SVar(2)}, SVar(2)],
         # This fails if metavar instantiations are not propagated into the plug
-        [lambda mvar: SSubst(mvar, SVar(0), mvar), MetaVar(0), {0: SVar(0)}, SVar(0)],
+        [lambda mvar: SSubst(mvar, SVar(0), mvar), phi0, {0: SVar(0)}, SVar(0)],
     ],
 )
 def test_instantiate_ssubst(
