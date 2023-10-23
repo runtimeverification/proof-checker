aa137ed5e4efec0e7b03c8e8ad0261f3163c3cf7
Bruce Collie <brucecollie82@gmail.com>
2023-10-23 16:54:31 +0100

    Update parser and generated hints

--- generation/src/rewrite/llvm_proof_hint.py
+++ generation/src/rewrite/llvm_proof_hint.py
@@ -8,6 +8,8 @@ from pyk.kllvm import ast as kllvm_kore
 from pyk.kllvm.convert import llvm_to_kore
 
 if TYPE_CHECKING:
+    from typing import Final
+
     from pyk.kore.syntax import Pattern
 
 
@@ -19,9 +21,16 @@ class LLVMRewriteStep:
 
 
 @dataclass
+class LLVMFunctionEvent:
+    name: str
+    relative_position: str
+
+
+@dataclass
 class LLVMRewriteTrace:
     initial_config: Pattern
     trace: tuple[LLVMRewriteStep, ...]
+    function_events: tuple[LLVMFunctionEvent, ...]
 
     @staticmethod
     def parse(input: bytes) -> LLVMRewriteTrace:
@@ -43,38 +52,56 @@ class LLVMRewriteTraceParser:
     proof_trace := initial_config rewrite_trace*
     """
 
+    end_sentinel: Final = bytes([0xCC] * 8)
+    function_event_sentinel: Final = bytes([0xDD] * 8)
+    kore_sentinel: Final = bytes([0xFF] * 8)
+
     def __init__(self, input: bytes):
         self.input = input
 
     def read_rewrite_trace(self) -> LLVMRewriteTrace:
-        init_config = self.read_guarded_term()
+        events, init_config = self.read_initial_config()
         traces: tuple[LLVMRewriteStep, ...] = ()
+
         while self.input:
-            traces = traces + (self.read_rewrite_step(),)
-        return LLVMRewriteTrace(init_config, traces)
+            maybe_event = self.try_read_function_event()
+            if maybe_event is not None:
+                events = events + (maybe_event,)
+            else:
+                traces = traces + (self.read_rewrite_step(),)
+
+        return LLVMRewriteTrace(init_config, traces, events)
+
+    def read_initial_config(self) -> tuple[tuple[LLVMFunctionEvent, ...], Pattern]:
+        events: tuple[LLVMFunctionEvent, ...] = ()
+
+        while self.input:
+            maybe_event = self.try_read_function_event()
+            if maybe_event is not None:
+                events = events + (maybe_event,)
+            else:
+                break
+
+        return (events, self.read_guarded_term())
 
     def read_guarded_term(self) -> Pattern:
-        self.read_constant(bytes([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]))
+        self.read_constant(self.kore_sentinel)
         return self.read_term_with_sentinel()
 
     def read_term_with_sentinel(self) -> Pattern:
-        sentinal = bytes(
-            [
-                0xCC,
-                0xCC,
-                0xCC,
-                0xCC,
-                0xCC,
-                0xCC,
-                0xCC,
-                0xCC,
-            ]
-        )
-        raw_term = self.read_until(sentinal)
-        self.read_constant(sentinal)
+        raw_term = self.read_until(self.end_sentinel)
+        self.read_constant(self.end_sentinel)
         llvm_pattern = kllvm_kore.Pattern.deserialize(raw_term)
         assert llvm_pattern, ('Could not deserialize binary kore.', input)
         return llvm_to_kore(llvm_pattern)
+
+    def try_read_function_event(self) -> LLVMFunctionEvent | None:
+        if self.try_read_constant(self.function_event_sentinel):
+            name = self.read_c_string()
+            position = self.read_c_string()
+            return LLVMFunctionEvent(name, position)
+
+        return None
 
     def read_rewrite_step(self) -> LLVMRewriteStep:
         ordinal = self.read_uint64()
@@ -90,13 +117,23 @@ class LLVMRewriteTraceParser:
         return LLVMRewriteStep(ordinal, substitution, term)
 
     def read_variable_name(self) -> str:
+        return self.read_c_string()
+
+    def read_c_string(self) -> str:
         ret = str(self.read_until(b'\x00'), 'ascii')
         self.read_constant(b'\x00')
         return ret
 
+    def try_read_constant(self, constant: bytes) -> bool:
+        if self.input[: len(constant)] == constant:
+            self.input = self.input[len(constant) :]
+            return True
+
+        return False
+
     def read_constant(self, constant: bytes) -> None:
-        assert self.input[: len(constant)] == constant
-        self.input = self.input[len(constant) :]
+        success = self.try_read_constant(constant)
+        assert success
 
     def read_uint64(self) -> int:
         index = 8
