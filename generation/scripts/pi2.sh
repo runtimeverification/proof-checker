4eb048454882dd90a367e77debc47b0c6230cfee
Adam Fiedler <info@fiedlr.sk>
2023-11-09 13:01:57 +0100

    Fixing a few things

--- generation/scripts/pi2.sh
+++ generation/scripts/pi2.sh
@@ -4,12 +4,17 @@
 # 1) generates a matching logic proof for a concrete execution of a program
 # 2) generates a ZK certificate for this proof
 #
-#
 # Note: It assumes an installed, recent-enough version of K that is capable of
-# producing proof hints
+# producing proof hints, and running from the generation folder.
 #
 ##############################################################################
 
-./gen-execution-proof-hints.sh $1 $2 $3
+./scripts/gen-execution-proof-hints.sh $1 $2 $3/proof-hint.bin
 
-poetry -C generation run python -m "kore_transfer.proof_gen" $1 $3 .build/kompiled-definitions/$1-kompiled --clean --proof-dir ./
+if [[ "$4" == "--pretty" ]]; then
+  OPTS="--pretty"
+else
+  OPTS=""
+fi
+
+poetry -C generation run python -m "kore_transfer.proof_gen" $1 $3/proof-hint.bin $3 --reuse --proof-dir ./ $OPTS
