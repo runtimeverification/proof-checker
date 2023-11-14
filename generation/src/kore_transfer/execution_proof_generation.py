ed37f7db57a34f0c5d98a2ca83ac4800842d8c9a
Ilja Zakharov <ilja.zakharov@runtimeverification.com>
2023-11-14 11:12:44 +0100

    Merge two files related to proof generation

--- generation/src/kore_transfer/execution_proof_generation.py
+++ generation/src/kore_transfer/execution_proof_generation.py
@@ -4,9 +4,12 @@ from collections.abc import Callable
 from typing import TYPE_CHECKING
 
 import proof_generation.proof as proof
+import proof_generation.proofs.kore_lemmas as kl
 from kore_transfer.language_semantics import KRewritingRule
 
 if TYPE_CHECKING:
+    from collections.abc import Iterator
+
     from kore_transfer.generate_hints import RewriteStepExpression
     from kore_transfer.language_semantics import LanguageSemantics
     from proof_generation.pattern import Pattern
@@ -31,3 +34,20 @@ class KoreDefinition(proof.ProofExp):
         assert len(self._axioms) > 0, 'No axioms to prove the rewrite step'
         self._claims.append(claim)
         self._proof_expressions.append(self.dynamic_inst(self.load_axiom(axiom), instantiations))
+
+
+def generate_proofs(hints: Iterator[RewriteStepExpression], language_semantics: LanguageSemantics) -> KoreDefinition:
+    proof_expression = KoreDefinition()
+    claims = 0
+    for hint in hints:
+        axiom = proof_expression.add_axioms(hint, language_semantics)
+        assert isinstance(axiom, KRewritingRule)
+        rewrite_axiom = axiom.pattern
+        sort, _, _ = kl.kore_rewrites.assert_matches(rewrite_axiom)
+        claim = kl.kore_rewrites(sort, hint.configuration_before, hint.configuration_after)
+
+        proof_expression.prove_rewrite_step(claim, rewrite_axiom, hint.substitutions)
+        claims += 1
+
+    print(f'Generated {claims} claims')
+    return proof_expression
