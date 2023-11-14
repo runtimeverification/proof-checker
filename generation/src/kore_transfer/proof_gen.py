a697b5ad89b25476073b9a192e4da441d41f97dd
Ilja Zakharov <ilja.zakharov@runtimeverification.com>
2023-11-14 09:24:52 +0100

    Add an option for pretty printing proofs

--- generation/src/kore_transfer/proof_gen.py
+++ generation/src/kore_transfer/proof_gen.py
@@ -39,11 +39,12 @@ def get_kompiled_dir(k_file: str, output_dir: str, reuse_kompiled_dir: bool = Fa
     return kompiled_dir
 
 
-def generate_proof_file(proof_gen: ProofExp, output_dir: Path, file_name: str) -> None:
+def generate_proof_file(proof_gen: ProofExp, output_dir: Path, file_name: str, pretty: bool = False) -> None:
     """Generate the proof files."""
     if not output_dir.exists():
         output_dir.mkdir(parents=True)
-    proof_gen.main(['', 'binary', str(output_dir), file_name])
+    mode = 'pretty' if pretty else 'binary'
+    proof_gen.main(['', mode, str(output_dir), file_name])
 
 
 HINTS_DIR_PATH = 'proof-hints'
@@ -67,6 +68,7 @@ def main(
     hints_file: str,
     output_dir: str,
     proof_dir: str,
+    pretty: bool = False,
     reuse_kompiled_dir: bool = False,
     rewrite_proof_files: bool = False,
 ) -> None:
@@ -91,9 +93,15 @@ if __name__ == '__main__':
     argparser.add_argument('kfile', type=str, help='Path to the K definition file')
     argparser.add_argument('hints', type=str, help='Path to the binary hints file')
     argparser.add_argument('output_dir', type=str, help='Path to the output directory')
+    argparser.add_argument(
+        '--pretty',
+        action='store_true',
+        default=False,
+        help='Print the pretty-printed version of proofs instead of the binary ones',
+    )
     argparser.add_argument('--reuse', action='store_true', default=False, help='Reuse the existing kompiled directory')
     argparser.add_argument('--clean', action='store_true', default=False, help='Rewrite proofs if they already exist')
     argparser.add_argument('--proof-dir', type=str, default=str(Path.cwd()), help='Output directory for saving proofs')
 
     args = argparser.parse_args()
-    main(args.kfile, args.hints, args.output_dir, args.proof_dir, args.reuse, args.clean)
+    main(args.kfile, args.hints, args.output_dir, args.proof_dir, args.pretty, args.reuse, args.clean)
