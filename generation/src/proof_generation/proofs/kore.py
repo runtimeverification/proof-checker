ec819732b0ccd22cb0dd0f34ff4603687e46bee3
Adam Fiedler <info@fiedlr.sk>
2023-11-15 23:43:25 +0100

    Add a few fixes for notations (#447)

--- generation/src/proof_generation/proofs/kore.py
+++ generation/src/proof_generation/proofs/kore.py
@@ -20,13 +20,14 @@ phi2 = MetaVar(2)
 inhabitant_symbol = Symbol('inhabitant')
 kore_next_symbol = Symbol('kore_next')
 kore_dv_symbol = Symbol('kore_dv')
+kore_kseq_symbol = Symbol('kore_kseq')
 
 
 """ kore_top(sort) """
-kore_top = Notation('kore-top', 1, App(inhabitant_symbol, phi0), 'k{0}[⊤]')
+kore_top = Notation('kore-top', 1, App(inhabitant_symbol, phi0), 'k⊤:{0}')
 
 """ kore_not(sort, pattern) """
-kore_not = Notation('kore-not', 2, _and(neg(phi1), kore_top(phi0)), 'k¬{0}[{1}]')
+kore_not = Notation('kore-not', 2, _and(neg(phi1), kore_top(phi0)), '(k¬{1}):{0}')
 
 """ kore_and(sort, pattern, pattern) """
 kore_and = Notation('kore-and', 3, _and(phi1, phi2), '({1} k⋀ {2})')
@@ -35,16 +36,20 @@ kore_and = Notation('kore-and', 3, _and(phi1, phi2), '({1} k⋀ {2})')
 kore_or = Notation('kore-or', 3, _or(phi1, phi2), '({1} k⋁ {2})')
 
 """ kore_next(sort, pattern) """
-kore_next = Notation('kore-next', 2, App(kore_next_symbol, phi1), '♦{0}')
+kore_next = Notation('kore-next', 2, App(kore_next_symbol, phi1), '♦{1}')
 
 """ kore_implies(sort, pattern, pattern) """
-kore_implies = Notation('kore-implies', 3, kore_or(phi0, kore_not(phi0, phi1), phi2), '({0}[{1}] k-> {0}[{2}])')
+kore_implies = Notation('kore-implies', 3, kore_or(phi0, kore_not(phi0, phi1), phi2), '({1} k-> {2}):{0}')
 
 """ kore_rewrites(sort, left, right) """
-kore_rewrites = Notation('kore-rewrites', 3, kore_implies(phi0, phi1, kore_next(phi0, phi2)), '({0}[{1}] k=> {0}[{2}])')
+kore_rewrites = Notation('kore-rewrites', 3, kore_implies(phi0, phi1, kore_next(phi0, phi2)), '({1} k=> {2}):{0}')
 
 """ kore_dv(sort, value) """
 kore_dv = Notation('kore-dv', 2, App(App(kore_dv_symbol, phi0), phi1), '{1}:{0}')
+
+# TODO: Add support for multiple apps of kseq without brackets
+""" kore_kseq(left, right) """
+kore_kseq = Notation('kore-kseq', 2, App(App(kore_kseq_symbol, phi0), phi1), '({0} ~> {1})')
 
 
 # We can do that without worrying about the memory leaking because all notations are saved in the ProofExp object anyway.
