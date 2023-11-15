7d060bdd1eabfc16bf954dfb7a4688fd31acbb30
Ilja Zakharov <ilja.zakharov@runtimeverification.com>
2023-11-15 17:34:50 +0100

    Use notations in ExecutionProofExp

--- generation/src/kore_transfer/execution_proof_generation.py
+++ generation/src/kore_transfer/execution_proof_generation.py
@@ -6,21 +6,20 @@ from typing import TYPE_CHECKING
 import proof_generation.proof as proof
 import proof_generation.proofs.kore as kl
 from kore_transfer.kore_convertion.language_semantics import KRewritingRule
-from proof_generation.proofs.kore import KORE_NOTATIONS
 
 if TYPE_CHECKING:
     from collections.abc import Iterator
 
     from kore_transfer.kore_convertion.language_semantics import LanguageSemantics
     from kore_transfer.kore_convertion.rewrite_steps import RewriteStepExpression
-    from proof_generation.pattern import Pattern
+    from proof_generation.pattern import Notation, Pattern
 
 ProofMethod = Callable[[proof.ProofExp], proof.ProofThunk]
 
 
 class ExecutionProofExp(proof.ProofExp):
-    def __init__(self) -> None:
-        super().__init__(notations=list(KORE_NOTATIONS))
+    def __init__(self, notations: list[Notation]) -> None:
+        super().__init__(notations=notations)
 
     def add_axioms(self, hint: RewriteStepExpression, language_semantics: LanguageSemantics) -> KRewritingRule:
         """Add axioms to the definition."""
@@ -38,7 +37,7 @@ class ExecutionProofExp(proof.ProofExp):
 
 
 def generate_proofs(hints: Iterator[RewriteStepExpression], language_semantics: LanguageSemantics) -> ExecutionProofExp:
-    proof_expression = ExecutionProofExp()
+    proof_expression = ExecutionProofExp(list(language_semantics.notations))
     claims = 0
     for hint in hints:
         axiom = proof_expression.add_axioms(hint, language_semantics)
