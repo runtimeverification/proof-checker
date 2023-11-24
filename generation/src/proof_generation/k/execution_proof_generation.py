e8af6fe82b1e43ba086fea3c4628f215cf08e97a
Octavian Mircea Sebe <mirceasebe@yahoo.com>
2023-11-24 17:45:17 +0100

    implemented module compositionality for ProofExp classes

--- generation/src/proof_generation/k/execution_proof_generation.py
+++ generation/src/proof_generation/k/execution_proof_generation.py
@@ -6,6 +6,7 @@ import proof_generation.proof as proof
 import proof_generation.proofs.kore as kl
 from proof_generation.k.kore_convertion.language_semantics import AxiomType, ConvertedAxiom, KRewritingRule
 from proof_generation.pattern import Symbol
+from proof_generation.proofs.definedness import functional
 
 if TYPE_CHECKING:
     from collections.abc import Iterator
@@ -39,7 +40,7 @@ class ExecutionProofExp(proof.ProofExp):
             k_sym = language_semantics.resolve_to_ksymbol(sym)
             assert k_sym is not None
             assert k_sym.is_functional
-            converted_pattern = kl.functional(pattern)
+            converted_pattern = functional(pattern)
             subst_axioms.append(ConvertedAxiom(AxiomType.FunctionalSymbol, converted_pattern))
         return subst_axioms
 
--- generation/src/proof_generation/proofs/kore.py
+++ generation/src/proof_generation/proofs/kore.py
@@ -4,21 +4,7 @@ import sys
 from functools import cache
 from typing import TYPE_CHECKING
 
-from proof_generation.pattern import (
-    App,
-    EVar,
-    Exists,
-    Implies,
-    Instantiate,
-    MetaVar,
-    Notation,
-    Symbol,
-    _and,
-    _or,
-    equiv,
-    neg,
-    top,
-)
+from proof_generation.pattern import App, Instantiate, MetaVar, Notation, Symbol, _and, _or, neg
 from proof_generation.proof import ProofExp
 
 if TYPE_CHECKING:
@@ -27,13 +13,6 @@ if TYPE_CHECKING:
 phi0 = MetaVar(0)
 phi1 = MetaVar(1)
 phi2 = MetaVar(2)
-
-# TODO: Add these notations to a Definedness module that also contains the definedness axiom
-ceil = Notation('ceil', 1, App(top(), phi0), '⌈ {0} ⌉')
-floor = Notation('floor', 1, neg(ceil(neg(phi0))), '⌊ {0} ⌋')
-subset = Notation('subset', 2, floor(Implies(phi0, phi1)), '({0} ⊆ {1})')
-equals = Notation('equals', 2, floor(equiv(phi0, phi1)), '({0} = {1})')
-functional = Notation('functional', 1, Exists(0, equals(EVar(0), MetaVar(0, (EVar(0),)))), 'functional({0})')
 
 # TODO: Make sure this is handled uniformly
 inhabitant_symbol = Symbol('inhabitant')
@@ -113,11 +92,6 @@ KORE_NOTATIONS = (
     kore_rewrites,
     kore_dv,
     kore_kseq,
-    ceil,
-    floor,
-    subset,
-    equals,
-    functional,
 )
 
 
