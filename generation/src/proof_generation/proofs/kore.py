74f150c6431610c511c45b45b9587388b4f4daba
Ilja Zakharov <ilja.zakharov@runtimeverification.com>
2023-11-27 11:05:02 +0100

    Migrate more notations from Metamath implementation

--- generation/src/proof_generation/proofs/kore.py
+++ generation/src/proof_generation/proofs/kore.py
@@ -18,6 +18,7 @@ from proof_generation.pattern import (
     equiv,
     neg,
     top,
+    bot
 )
 from proof_generation.proof import ProofExp
 
@@ -29,18 +30,21 @@ phi1 = MetaVar(1)
 phi2 = MetaVar(2)
 phi3 = MetaVar(3)
 
+# TODO: Make sure this is handled uniformly
+inhabitant_symbol = Symbol('inhabitant')
+kore_next_symbol = Symbol('kore_next')
+kore_dv_symbol = Symbol('kore_dv')
+kore_kseq_symbol = Symbol('kore_kseq')
+
 # TODO: Add these notations to a Definedness module that also contains the definedness axiom
 ceil = Notation('ceil', 1, App(top(), phi0), '⌈ {0} ⌉')
 floor = Notation('floor', 1, neg(ceil(neg(phi0))), '⌊ {0} ⌋')
 subset = Notation('subset', 2, floor(Implies(phi0, phi1)), '({0} ⊆ {1})')
 equals = Notation('equals', 2, floor(equiv(phi0, phi1)), '({0} = {1})')
 functional = Notation('functional', 1, Exists(0, equals(EVar(0), MetaVar(0, (EVar(0),)))), 'functional({0})')
-
-# TODO: Make sure this is handled uniformly
-inhabitant_symbol = Symbol('inhabitant')
-kore_next_symbol = Symbol('kore_next')
-kore_dv_symbol = Symbol('kore_dv')
-kore_kseq_symbol = Symbol('kore_kseq')
+included = Notation('included', 2, floor(Implies(phi0, phi1)), '({0} ⊆ {1})')
+in_sort = Notation('in-sort', 2, included(phi0, App(inhabitant_symbol, phi1)), '({0} s∈ {1})')
+sorted_exists = Notation('sorted-exists', 3, Exists(phi0.name, _and(in_sort(phi0, phi1), phi2)), '( s∃ {0}:{1}. {2} )')
 
 
 """ kore_top(sort) """
@@ -77,12 +81,20 @@ kore_floor = Notation('kore-floor', 3, kore_not(phi1, kore_ceil(phi0, phi1, kore
 kore_iff = Notation('kore-iff', 3, kore_and(phi0, kore_implies(phi0, phi1, phi2), kore_implies(phi0, phi2, phi1)), '({1} k<-> {2}):{0}')
 
 """ kore_equals(sort, left, right) """
-kore_equals = Notation('kore-equals', 4, kore_floor(phi0, phi1, kore_iff(phi0, phi2, phi3)), '({1} k= {2}):{0}')
+kore_equals = Notation('kore-equals', 4, kore_floor(phi0, phi1, kore_iff(phi0, phi2, phi3)), '({1}:{0} k= {2}:{0}):{1}')
 
 # TODO: Add support for multiple apps of kseq without brackets
 """ kore_kseq(left, right) """
 kore_kseq = Notation('kore-kseq', 2, App(App(kore_kseq_symbol, phi0), phi1), '({0} ~> {1})')
 
+""" kore_in(sort, outer_sort, left, right) """
+kore_in = Notation('kore-in', 4, kore_floor(phi0, phi1, kore_implies(phi0, phi2, phi3)), '({2}:{0}} k∈ {3}:{0}):{1}')
+
+""" kore_bottom(sort) """
+kore_bottom = Notation('kore-bottom', 1, bot(), 'k⊥:{0}')
+
+""" kore_exists(evar_sort, outer_sort, evar, pattern) """
+kore_exists = Notation('kore-exists', 4, _and(sorted_exists(phi2, phi0, phi3), App(inhabitant_symbol, phi1)), '( k∃ {3}:{0}. {3}):{1}')
 
 # We can do that without worrying about the memory leaking because all notations are saved in the ProofExp object anyway.
 # Note that @cache is required here as we have to return the same objects for the same arguments for notation comparisons
