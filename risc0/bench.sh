12fb6ce4f133c0ef64851c4c08934a9da0ddfd5a
Roberto Rosmaninho <robertogomes@dcc.ufmg.br>
2023-12-03 20:18:03 +0000

    Fixing `bench.sh` and disable cuda form risc0 csl-examples

--- risc0/bench.sh
+++ risc0/bench.sh
@@ -1,20 +1,67 @@
 #!/bin/bash
-# Usage: bench.sh <output file>
+# Usage: bench.sh <output file> [example name]
 
+if [ -z "$1" ]; then
+    echo "Usage: bench.sh <output file> [example name]"
+    exit 1
+fi
+
+output_file="$1"
+
+# print_output
+print_output() {
+    echo "$1" | grep -E "R0VM\[" | awk -F "] " '{print $2}' >> "$output_file"
+    echo "$1" | grep -E "Running execution \+ ZK certficate generation \+ verification" >> "$output_file"
+    echo "---------------------------------------------------------------------" >> "$output_file"
+}
+
+# svm5
+svm5() {
+    echo "Running svm5"
+    cp methods/guest/src/svm5.rs methods/guest/src/main.rs
+    output=$(RUST_LOG=info cargo run)
+    print_output "$output"
+}
+
+# perceptron
+perceptron() {
+    echo "Running perceptron"
+    cp methods/guest/src/perceptron.rs methods/guest/src/main.rs
+    output=$(RUST_LOG=info cargo run)
+    print_output "$output"
+}
+
+# transfer
+transfer() {
+    echo "Running transfer"
+    cp methods/guest/src/transfer.rs methods/guest/src/main.rs
+    output=$(RUST_LOG=info cargo run)
+    print_output "$output"
+}
+
+# transfer5000
+transfer5000() {
+    echo "Running transfer5000"
+    cp methods/guest/src/transfer5000.rs methods/guest/src/main.rs
+    output=$(RUST_LOG=info cargo run)
+    print_output "$output"
+}
+
+all() {
+    echo "Running all examples"
+    svm5
+    perceptron
+    transfer
+    transfer5000
+}
 
 cd csl-examples
 
-for file in methods/guest/src/*.rs; do
-    echo $file
-    cp "$file" methods/guest/src/main.rs
-
-    output=$(RUST_LOG=info cargo run)
-
-    echo "$output" | grep -E "R0VM\[" | awk -F "] " '{print $2}' >> "$1"
-    echo "$output" | grep -E "Running execution \+ ZK certficate generation \+ verification" >> "$1"
-    echo "---------------------------------------------------------------------" >> "$1"
-
-done
+if [ -z "$2" ]; then
+    all
+else
+    "$2"
+fi
 
 rm methods/guest/src/main.rs
 mv "$1" ../"$1"
