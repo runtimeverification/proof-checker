4f299bc41c30b5aac767679a17ae4111dc9076ed
musab.alturki <musab.alturki@gmail.com>
2023-12-05 14:13:54 -0600

    Add a memoizer unit test

---
+++ generation/src/tests/unit/test_memoizing_interpreter.py
@@ -0,0 +1,23 @@
+from proof_generation.basic_interpreter import ExecutionPhase
+from proof_generation.instantiation_optimizer import MemoizingInterpreter
+from proof_generation.pattern import MetaVar
+from proof_generation.stateful_interpreter import StatefulInterpreter
+
+
+def test_memoizing_interpreter() -> None:
+    sub_interpreter = StatefulInterpreter(ExecutionPhase.Proof)
+    memoizer = MemoizingInterpreter(sub_interpreter, {MetaVar(0), MetaVar(1)})
+
+    memoizer.metavar(0)
+    assert sub_interpreter.stack[-1] == MetaVar(0)
+    assert isinstance(sub_interpreter.stack[-1], MetaVar)
+
+    cached_pattern = memoizer.pattern(MetaVar(1))
+    assert isinstance(cached_pattern, MetaVar)
+    assert cached_pattern.name == 1
+    assert cached_pattern in sub_interpreter.memory
+
+    cached_pattern = memoizer.pattern(MetaVar(2))
+    assert isinstance(cached_pattern, MetaVar)
+    assert cached_pattern.name == 2
+    assert cached_pattern not in sub_interpreter.memory
