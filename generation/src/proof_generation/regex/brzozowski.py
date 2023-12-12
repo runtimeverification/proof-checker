eba3538fe5707d1d1139672814b9ba11804e2055
Nishant Rodrigues <nishantjr@gmail.com>
2023-12-12 18:04:09 +0530

    make format; make check

--- generation/src/proof_generation/regex/brzozowski.py
+++ generation/src/proof_generation/regex/brzozowski.py
@@ -1,15 +1,32 @@
-from .regex import Regex, Kleene, Choice, Concat, implies, Letter, a, b, EmptySet, Epsilon, Not, less_than
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from .regex import Choice, Concat, EmptySet, Epsilon, Kleene, Letter, Not, a, b, less_than
+
+if TYPE_CHECKING:
+    from .regex import Regex
+
 
 def has_ewp(exp: Regex) -> bool:
     match exp:
-        case EmptySet(): return False
-        case Epsilon(): return True
-        case Letter(_): return False
-        case Concat(l, r):  return has_ewp(l) and has_ewp(r)
-        case Choice(l, r): return has_ewp(l) or has_ewp(r)
-        case Kleene(_): return True
-        case Not(e): return not has_ewp(e)
-        case _: raise AssertionError(exp)
+        case EmptySet():
+            return False
+        case Epsilon():
+            return True
+        case Letter(_):
+            return False
+        case Concat(l, r):
+            return has_ewp(l) and has_ewp(r)
+        case Choice(l, r):
+            return has_ewp(l) or has_ewp(r)
+        case Kleene(_):
+            return True
+        case Not(e):
+            return not has_ewp(e)
+        case _:
+            raise AssertionError(exp)
+
 
 def left_assoc(exp: Regex) -> Regex:
     match exp:
@@ -23,33 +40,50 @@ def left_assoc(exp: Regex) -> Regex:
         case Choice(e1, e2):
             return Choice(e1, left_assoc(e2))
 
-        case Kleene(e): return Kleene(left_assoc(e))
-        case Not(e): return Not(left_assoc(e))
+        case Kleene(e):
+            return Kleene(left_assoc(e))
+        case Not(e):
+            return Not(left_assoc(e))
 
-        case _: return exp
+        case _:
+            return exp
+
 
 def identities(exp: Regex) -> Regex:
     match exp:
-        case Concat(EmptySet(), e2): return EmptySet()
-        case Concat(e1, EmptySet()): return EmptySet()
-        case Concat(Epsilon(), e2): return e2
-        case Concat(e1, Epsilon()): return e1
+        case Concat(EmptySet(), e2):
+            return EmptySet()
+        case Concat(e1, EmptySet()):
+            return EmptySet()
+        case Concat(Epsilon(), e2):
+            return e2
+        case Concat(e1, Epsilon()):
+            return e1
         case Concat(e1, e2):
             return Concat(identities(e1), identities(e2))
 
-        case Choice(e1, EmptySet()): return identities(e1)
-        case Choice(EmptySet(), e1): return identities(e1)
-        case Choice(e1, Choice(e2, e3)) if e1 == e2: return Choice(e1, e3)
-        case Choice(e1, e2) if e1 == e2: return e1
+        case Choice(e1, EmptySet()):
+            return identities(e1)
+        case Choice(EmptySet(), e1):
+            return identities(e1)
+        case Choice(e1, Choice(e2, e3)) if e1 == e2:
+            return Choice(e1, e3)
+        case Choice(e1, e2) if e1 == e2:
+            return e1
         case Choice(e1, e2):
             return Choice(identities(e1), identities(e2))
 
-        case Kleene(Kleene(e)): return identities(Kleene(e))
-        case Kleene(e): return Kleene(identities(e))
+        case Kleene(Kleene(e)):
+            return identities(Kleene(e))
+        case Kleene(e):
+            return Kleene(identities(e))
 
-        case Not(e): return Not(identities(e))
+        case Not(e):
+            return Not(identities(e))
 
-        case _: return exp
+        case _:
+            return exp
+
 
 def sort_choice(exp: Regex) -> Regex:
     match exp:
@@ -60,9 +94,13 @@ def sort_choice(exp: Regex) -> Regex:
                 return Choice(e1, sort_choice(Choice(e2, e3)))
             else:
                 return Choice(e2, sort_choice(Choice(e1, e3)))
-        case Kleene(e): return Kleene(sort_choice(e))
-        case Not(e): return Not(sort_choice(e))
-        case _: return exp
+        case Kleene(e):
+            return Kleene(sort_choice(e))
+        case Not(e):
+            return Not(sort_choice(e))
+        case _:
+            return exp
+
 
 def normalize(exp: Regex) -> Regex:
     prev = None
@@ -91,12 +129,14 @@ def derivative(by: Letter, exp: Regex) -> Regex:
                 return normalize(Choice(Concat(derivative(by, l), r), derivative(by, r)))
             else:
                 return normalize(Concat(derivative(by, l), r))
-        case Choice(l, r): return normalize(Choice(derivative(by, l), derivative(by, r)))
+        case Choice(l, r):
+            return normalize(Choice(derivative(by, l), derivative(by, r)))
         case Kleene(e):
             return normalize(Concat(derivative(by, e), Kleene(e)))
         case Not(e):
             return normalize(Not(derivative(by, e)))
-        case _: raise AssertionError
+        case _:
+            raise AssertionError
 
 
 class BrzInstumentation:
@@ -109,7 +149,9 @@ class BrzInstumentation:
     def leaf(self, index: int) -> None:
         pass
 
+
 null_instr = BrzInstumentation()
+
 
 def brzozowski(exp: Regex, instr: BrzInstumentation, prev: list[Regex] | None = None) -> bool:
     if prev == None:
@@ -131,4 +173,3 @@ def brzozowski(exp: Regex, instr: BrzInstumentation, prev: list[Regex] | None = 
     prev.pop()
     instr.exit_node()
     return left and right
-
