e2107ed6dd564f2c3780cc7b7e785874862da6bb
Ilja Zakharov <ilja.zakharov@runtimeverification.com>
2023-12-18 10:51:52 +0100

    Remove the misleading comment as we don't use the equality for configurations

--- generation/src/proof_generation/k/execution_proof_generation.py
+++ generation/src/proof_generation/k/execution_proof_generation.py
@@ -57,7 +57,6 @@ class SimplificationProver(proof.ProofExp):
         )
 
     def equality_transitivity(self, last_proof: proof.ProofThunk, new_proof: proof.ProofThunk) -> proof.ProofThunk:
-        # Apply transitivity for configurations
         return self.kore_lemmas.sorted_eq_trans(last_proof, new_proof)
 
     def prove_substitutions(self, pattern: proof.ProofThunk, substitutions: dict[int, Pattern]) -> proof.ProofThunk:
